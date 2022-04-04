8af3c4a52d82ab8af5e961bfb6e4dbaa59c3732e
Dmitry Sidortsov <dmx3214@mail.ru>
2022-04-04 19:58:05 +0300

    tytyjyt

--- vue-todo/src/components/Todos.vue
+++ vue-todo/src/components/Todos.vue
@@ -1,34 +1,43 @@
 <template>
   <div v-if="todos.length > 0">
     <div>
-      <TodoItem 
-      v-bind:key="todo.id"
-      v-for="(todo, i) in todos" 
-      v-bind:index='i'
-      v-bind:id="todo.id" 
-      v-bind:title="todo.title"
-      v-bind:todo="todo" 
-      v-on:del-todo="$emit('del-todo', todo.id)"
-      v-bind:todos="todos"/>
+      <TodoItem
+        v-bind:key="todo.id"
+        v-for="(todo, i) in todos"
+        v-bind:index="i"
+        v-bind:id="todo.id"
+        v-bind:title="todo.title"
+        v-bind:todo="todo"
+        v-on:del-todo="$emit('del-todo', todo.id)"
+        v-bind:todos="todos"
+      />
     </div>
+    <h3>
+      Общее количество: <b>{{ updateCount }}</b>
+    </h3>
   </div>
   <h2 v-else class="text__no__tasks">Список пуск</h2>
-  <hr>
+  <hr />
 </template>
 
 <script>
-import TodoItem from './TodoItem.vue';
+import TodoItem from "./TodoItem.vue";
 export default {
   name: "Todos-app",
   components: {
-    TodoItem
+    TodoItem,
   },
-  props: ["todos"]
-}
+  props: ["todos"],
+  computed: {
+    updateCount() {
+      return this.todos.length
+    }
+  }
+};
 </script>
 
 <style scoped>
-.text__no__tasks{
+.text__no__tasks {
   text-align: center;
   color: brown;
   margin-top: 10px;
